a21e61c6844c39a409cfb815573a12b933941b61
Hasanali Asadov <125257868+hasanasadov@users.noreply.github.com>
2024-10-12 03:01:33 +0400

    Update ToDoEl.jsx

--- src/Comp/ToDoEl.jsx
+++ src/Comp/ToDoEl.jsx
@@ -18,7 +18,6 @@ function ToDoEl({ id, checked, task, hide, handleAction }) {
 
   //! ------------------ Set States ------------------ !//
   const handleEditClick = () => {
-    inputRef.focus();
     setClickOnEdit(true);
   };
 
